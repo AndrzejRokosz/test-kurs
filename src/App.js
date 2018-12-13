42b6726b8674175996fa5b0a8a9dd8efdbabf8ef
Andrzej Rokosz <andrzej_ro@wp.pl>
2018-12-13 09:56:23 +0100

    working

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
 class App extends Component {
+  state = {
+    data: null
+  }
+
+  componentDidMount() {
+    fetch("https://randomuser.me/api?results=10")
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          data: data.results
+        })
+      )
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {this.state.data
+          ? this.state.data.map
+            ? this.state.data.map(user => (
+              <div
+                key={user.email}
+              >
+                <img src={user.picture.large} alt={""} />
+                <div> {`${user.name.first} ${user.name.last}`}</div>
+                <a href={user.email}> {user.email}</a>
+                <hr />
+              </div>
+            ))
+            : "Something went wrong"
+          : "Be patient..."}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
